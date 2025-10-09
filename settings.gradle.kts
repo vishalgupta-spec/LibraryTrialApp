980b94e605b0fa676918b7ebdfd23100952bb631
PRANAY <pranaypatil.ext@innov.in>
2025-10-09 17:14:18 +0530

    vishal test2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,29 @@ dependencyResolutionManagement {
     }
 }
 
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "LibraryTrialApp"
 include(":app")
 include(":librarytrial")
+
